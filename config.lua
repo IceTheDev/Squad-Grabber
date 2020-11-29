2eb9e515fa84423b42a729e39cc5546dbecf530c
IceTheDev <admin@txdps-rp.com>
2020-11-28 19:31:15 -0600

    .

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 config = {
     squad_vehicle = {
         "f550ssquad"
-    }, -- If you changed the name from f550rb to something else, change that here!
+    }, 
     labelText = "~g~Open~w~ compartment doors.", -- This text displays on a label. (only if FloatingText is true)
     labelText2 = "~g~Close~w~ compartment doors." -- This text displays on a label. (only if FloatingText is true)
 }
