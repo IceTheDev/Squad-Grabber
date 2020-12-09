950276f77a41c825ec3a0bbe25f3edba41599e0b
IceTheDev <admin@txdps-rp.com>
2020-12-08 19:18:01 -0600

    pioneer light working, compartment doors almost working, but not quite.

--- client.lua
+++ client.lua
@@ -4,8 +4,10 @@ local lastTruckCoords = vector3(0,0,0)
 local r,g,b = 255,0,0
 
 local labels = {
-    {"REDNECK_HELP1", "Press ~INPUT_CONTEXT~ to ~g~open~w~ compartment doors.~n~Press ~INPUT_FRONTEND_SELECT~ to lift pioneers."},
-    {"REDNECK_HELP2", "Press ~INPUT_CONTEXT~ to ~r~close~w~ compartment doors.~n~Press ~INPUT_FRONTEND_SELECT~ to drop pioneers."}
+    {"REDNECK_HELP1", "~b~Instructional Buttons:~n~~w~Press ~INPUT_CONTEXT~ to ~g~open~w~ compartment doors.~n~Press ~INPUT_DETONATE~ to lift pioneers."},
+    {"REDNECK_HELP2", "~b~Instructional Buttons:~n~~w~Press ~INPUT_CONTEXT~ to ~r~close~w~ compartment doors.~n~Press ~INPUT_DETONATE~ to lift pioneers."},
+    {"REDNECK_HELP3", "~b~Instructional Buttons:~n~~w~Press ~INPUT_CONTEXT~ to ~g~open~w~ compartment doors.~n~Press ~INPUT_DETONATE~ to drop pioneers."},
+    {"REDNECK_HELP4", "~b~Instructional Buttons:~n~~w~Press ~INPUT_CONTEXT~ to ~r~close~w~ compartment doors.~n~Press ~INPUT_DETONATE~ to drop pioneers."}
 }
 
 
@@ -36,33 +38,31 @@ Citizen.CreateThread(function()
         if IsPedInAnyVehicle(ped,true) == false then
             markerCoords = GetOffsetFromEntityInWorldCoords(lastTruck, -1.2, -4.75, 0.0)
             if GetDistanceBetweenCoords(pos,markerCoords) < 5 then
-                if not IsVehicleDoorFullyOpen(lastTruck, 2) and not IsVehicleDoorFullyOpen(lastTruck, 3) then
-                    BeginTextCommandDisplayHelp(labels[1][1])
+                if not IsVehicleDoorFullyOpen(lastTruck, 5) then
+                    BeginTextCommandDisplayHelp(labels[1][1]) -- open compartment
                     EndTextCommandDisplayHelp(0, 0, 1, -1)
                 else
-                    BeginTextCommandDisplayHelp(labels[2][1])
+                    BeginTextCommandDisplayHelp(labels[2][1]) -- close compartment
                     EndTextCommandDisplayHelp(0, 0, 1, -1)
                 end
                 if GetDistanceBetweenCoords(markerCoords,pos) < 10 then
-                    if IsVehicleDoorFullyOpen(lastTruck, 2) and IsVehicleDoorFullyOpen(lastTruck, 3) then
-                        if IsControlPressed(0, 51) then 
-                            SetVehicleDoorShut(lastTruck, 2, true)
-                            SetVehicleDoorShut(lastTruck, 3, true)
+                    if IsVehicleDoorFullyOpen(lastTruck, 5) then
+                        if IsControlJustPressed(0, 51) then 
+                            SetVehicleDoorShut(lastTruck, 5, false)
                         end
                     else
-                        if IsControlPressed(0, 51) then
-                            SetVehicleDoorOpen(lastTruck, 2, false, false)
-                            SetVehicleDoorOpen(lastTruck, 3, false, false)
+                        if IsControlJustPressed(0, 51) then
+                            SetVehicleDoorOpen(lastTruck, 5, false, false)
                         end
                     end
 
-                    if IsVehicleDoorFullyOpen(lastTruck, 5) then
-                        if IsControlPressed(0, 217) then
-                            SetVehicleDoorShut(lastTruck, 5, true)
+                    if IsVehicleDoorFullyOpen(lastTruck, 4) then
+                        if IsControlJustPressed(0, 47) then
+                            SetVehicleDoorShut(lastTruck, 4, false)
                         end
                     else
-                        if IsControlPressed(0, 217) then
-                            SetVehicleDoorOpen(lastTruck, 5, false, false)
+                        if IsControlJustPressed(0, 47) then
+                            SetVehicleDoorOpen(lastTruck, 4, false, false)
                         end
                     end
                 end
